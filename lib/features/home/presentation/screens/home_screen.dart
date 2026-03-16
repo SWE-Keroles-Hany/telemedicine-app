38f9b6b33187864a855007fa92416e5f3b8ba905
KerolesHany <keroleshany293@gmail.com>
2026-03-16 13:17:48 +0200

    home scree witout any thing

--- lib/features/home/presentation/screens/home_screen.dart
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -1,40 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:telemedicine/core/constants/constants_text.dart';
-import 'package:telemedicine/core/theme/color_manger.dart';
-import 'package:telemedicine/core/widgets/custom_app_label.dart';
-import 'package:telemedicine/features/home/presentation/widgets/home_app_bar.dart';
-import 'package:telemedicine/features/home/presentation/widgets/our_services.dart';
-import 'package:telemedicine/features/home/presentation/widgets/top_doctors.dart';
-
-import '../widgets/top_doctors_and_view_all_row.dart';
 
 class HomeScreen extends StatelessWidget {
-  static const String routeName = "HomeScreen";
   const HomeScreen({super.key});
-
+  static const String routeName = "HomeScreen";
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: ColorManger.backGroundColor,
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(56.h),
-          child: const HomeAppBar(),
-        ),
-        body: ListView(
-          padding: EdgeInsets.all(16.r),
-          children: [
-            CustomAppLabel(label: ConstantsText.ourServices),
-            SizedBox(height: 10.h),
-            SizedBox(height: 300.h, child: OurServices()),
-            SizedBox(height: 15.h),
-            TopDoctorsAndViewAllRow(),
-            SizedBox(height: 10.h),
-            SizedBox(height: 210.h, child: TopDoctors()),
-          ],
-        ),
-      ),
-    );
+    return Scaffold();
   }
 }
